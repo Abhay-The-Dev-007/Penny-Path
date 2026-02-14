2a353d6df9391755e57b83bd2f89e5fe7d289022
Abhay-The-Dev-007 <abhay87dev@gmail.com>
2026-02-14 22:33:10 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,13 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 // serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
